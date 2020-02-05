075d3680e16fd64baf9c4d7836a222b1d36ea677
cvtc-tom-smith <60593154+cvtc-tom-smith@users.noreply.github.com>
2020-02-05 14:19:12 -0600

    Initial commit

---
+++ MustardClinic/MustardClinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MustardClinic.Startup))]
+namespace MustardClinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
