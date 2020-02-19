216bc19ed7c73e5613e7b4896f1e0651d4625ce6
Anthony Guentner <aguenther1@student.cvtc.edu>
2020-02-19 13:34:41 -0600

    Added more context

--- MustardClinic/MustardClinic/Context/Doctor_DBContext.cs
+++ MustardClinic/MustardClinic/Context/Doctor_DBContext.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MustardClinic.Context
 {
-    public class Doctor_DBContext
+    public class Doctor
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public DateTime JoiningDate { get; set; }
         public int Age { get; set; }
+    }
+
+    public class DoctorDBContext : DbContext
+    {
+        public DoctorDBContext()
+        { }
+        public DbSet<Doctor> Doctors { get; set; }
     }
 }
