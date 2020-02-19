b1e2f7128948d24341b810bb465eb2a44b45e6b0
Anthony Guentner <aguenther1@student.cvtc.edu>
2020-02-19 13:27:35 -0600

    Added DB context files

---
+++ MustardClinic/MustardClinic/Models/DoctorSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MustardClinic.Models
+{
+    public class DoctorSchedule
+    {
+        [Key]
+        public int DoctorId { get; set; }
+        public string MondayAvailability { get; set; }
+        public string TuesdayAvailability { get; set; }
+        public string WednesdayAvailability { get; set; }
+        public string ThursdayAvailability { get; set; }
+        public string FridayAvailability { get; set; }
+        public string SaturdayAvailability { get; set; }
+        public string SundayAvailability { get; set; }
+    }
+}
