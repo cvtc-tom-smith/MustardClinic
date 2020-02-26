e7030529eb54affc40b30c8d3eb94d5410796fed
Anthony Guentner <aguenther1@student.cvtc.edu>
2020-02-26 14:28:56 -0600

    bug fixes

--- MustardClinic/MustardClinic/Controllers/PrescriptionsController.cs
+++ MustardClinic/MustardClinic/Controllers/PrescriptionsController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MustardClinic.Models;
+using System.Net;
+using System.Data.Entity;
 
 namespace MustardClinic.Controllers
 {
